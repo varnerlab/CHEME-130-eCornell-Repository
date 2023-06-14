2465ccc24869f90ef37f1970fa172de96f987547
Jeffrey Varner <jdv27@cornell.edu>
2023-06-14 12:07:51 -0400

    module 1 ChE 132
    module 1 ChE 132

--- courses/CHEME-132/module-1/Include.jl
+++ courses/CHEME-132/module-1/Include.jl
@@ -12,9 +12,21 @@ using DataFrames
 using CSV
 using PrettyTables
 using Statistics
+using Plots
+using Colors
 
 # load my codes -
 include(joinpath(_PATH_TO_SRC, "Types.jl"))
 include(joinpath(_PATH_TO_SRC, "Factory.jl"))
 include(joinpath(_PATH_TO_SRC, "Compute.jl"))
 include(joinpath(_PATH_TO_SRC, "Files.jl"))
+
+# load colors -
+colors = Dict{Int64,RGB}()
+colors[1] = colorant"#EE7733";
+colors[2] = colorant"#0077BB";
+colors[3] = colorant"#33BBEE";
+colors[4] = colorant"#EE3377";
+colors[5] = colorant"#CC3311";
+colors[6] = colorant"#009988";
+colors[7] = colorant"#BBBBBB";
