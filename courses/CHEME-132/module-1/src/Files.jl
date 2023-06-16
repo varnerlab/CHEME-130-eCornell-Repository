60b024c035680969c6878a9583de660893cc2c21
Jeffrey Varner <jdv27@cornell.edu>
2023-06-15 22:59:38 -0400

    Module 1 Project
    Project for module 1

--- courses/CHEME-132/module-1/src/Files.jl
+++ courses/CHEME-132/module-1/src/Files.jl
@@ -9,3 +9,10 @@ function loaddatafile(; firm::Int = 1, year::String = "2022")::DataFrame
     # load the data 
     return CSV.read(path,DataFrame);
 end
+
+"""
+    loadmodelparametersfile() -> DataFrame
+"""
+function loadmodelparametersfile()::DataFrame
+    return CSV.read(joinpath(_PATH_TO_DATA,"Parameters.csv"), DataFrame);
+end
