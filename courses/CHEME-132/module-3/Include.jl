19c2ef8e08f010dc4f840731b2c8d659c37e12a5
Jeffrey Varner <jdv27@cornell.edu>
2023-06-21 20:09:13 -0500

    working
    ChE 132

--- courses/CHEME-132/module-3/Include.jl
+++ courses/CHEME-132/module-3/Include.jl
@@ -21,6 +21,7 @@ include(joinpath(_PATH_TO_SRC, "Types.jl"))
 include(joinpath(_PATH_TO_SRC, "Factory.jl"))
 include(joinpath(_PATH_TO_SRC, "Compute.jl"))
 include(joinpath(_PATH_TO_SRC, "Files.jl"))
+include(joinpath(_PATH_TO_SRC, "Visualize.jl"))
 
 # load colors -
 colors = Dict{Int64,RGB}()
