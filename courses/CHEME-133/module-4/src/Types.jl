e9a15938c8bb556c39118747f0074e56c20cc444
Jeffrey Varner <jdv27@cornell.edu>
2023-06-29 16:23:51 -0700

    Working on contracts code for module 4
    ChE 133 module 4 code

---
+++ courses/CHEME-133/module-4/src/Types.jl
@@ -0,0 +1,56 @@
+abstract type AbstractAssetModel end
+abstract type AbstractContractModel <: AbstractAssetModel end
+
+mutable struct MyEuropeanCallContractModel <: AbstractContractModel
+
+    # data -
+    K::Float64
+    sense::Int64
+    DTE::Float64
+    IV::Float64
+    premium::Union{Nothing, Float64}
+
+    # constructor -
+    MyEuropeanCallContractModel() = new()
+end
+
+mutable struct MyEuropeanPutContractModel <: AbstractContractModel
+
+    # data -
+    K::Float64
+    sense::Int64
+    DTE::Float64
+    IV::Float64
+    premium::Union{Nothing, Float64}
+
+    # constructor -
+    MyEuropeanPutContractModel() = new()
+end
+
+mutable struct MyAmericanCallContractModel <: AbstractContractModel
+
+    # data -
+    K::Float64
+    sense::Int64
+    DTE::Float64
+    IV::Float64
+    premium::Union{Nothing, Float64}
+    ticker::Union{Nothing,String}
+
+    # constructor -
+    MyAmericanCallContractModel() = new()
+end
+
+mutable struct MyAmericanPutContractModel <: AbstractContractModel
+
+    # data -
+    K::Float64
+    sense::Int64
+    DTE::Float64
+    IV::Float64
+    premium::Union{Nothing, Float64}
+    ticker::Union{Nothing,String}
+
+    # constructor -
+    MyAmericanPutContractModel() = new()
+end
