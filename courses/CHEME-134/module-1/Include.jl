e58bacfe64a0f47c12d6ea9a4fcb637e02eaf4b2
Jeffrey Varner <jdv27@cornell.edu>
2023-09-15 06:42:24 -0400

    134 options
    options

---
+++ courses/CHEME-134/module-1/Include.jl
@@ -0,0 +1,22 @@
+# setup paths -
+const _ROOT = pwd()
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+
+# download external packages
+import Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.update()
+
+# load packages -
+using VLQuantitativeFinancePackage
+using Plots
+using Colors
+using CSV
+using DataFrames
+using JLD2
+using FileIO
+using Statistics
+using Dates
+using LinearAlgebra
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Files.jl"))
