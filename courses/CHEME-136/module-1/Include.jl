50c5e68b19e7d796ce49c1f7b221fd60a0b4b435
Jeffrey Varner <jdv27@cornell.edu>
2024-01-04 16:20:34 -0500

    HMM module
    HMM module

---
+++ courses/CHEME-136/module-1/Include.jl
@@ -0,0 +1,14 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+
+# load external packages -
+using Distributions
+using Plots
+using Colors
+using LinearAlgebra
+using Statistics
+
+# load my codes -
+# ...
