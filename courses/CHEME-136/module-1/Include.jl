7b27795750dcd42e22881f2ebfd4c9b87f290317
Jeffrey Varner <jdv27@cornell.edu>
2024-01-05 13:04:55 -0500

    Hmmm
    not sure about this

--- courses/CHEME-136/module-1/Include.jl
+++ courses/CHEME-136/module-1/Include.jl
@@ -12,6 +12,9 @@ using Statistics
 using PrettyTables
 using JLD2
 using FileIO
+using StatsPlots
 
 # load my codes -
-# ...
+include(joinpath(_PATH_TO_SRC, "Types.jl"));
+include(joinpath(_PATH_TO_SRC, "Factory.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
