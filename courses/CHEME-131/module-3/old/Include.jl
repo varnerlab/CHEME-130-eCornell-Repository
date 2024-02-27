a47c816294060823cc53d7577aaffabc87d8a09a
Jeffrey Varner <jdv27@cornell.edu>
2024-02-27 05:27:55 -0500

    working with STRIPs
    STRIPs

---
+++ courses/CHEME-131/module-3/old/Include.jl
@@ -0,0 +1,16 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# load external pacakges -
+import Pkg; Pkg.activate("."); Pkg.instantiate();
+using Optim
+using PrettyTables
+using LinearAlgebra
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Types.jl"))
+include(joinpath(_PATH_TO_SRC, "Factory.jl"))
+include(joinpath(_PATH_TO_SRC, "Files.jl"))
+include(joinpath(_PATH_TO_SRC, "Compute.jl"))
+include(joinpath(_PATH_TO_SRC, "YTM.jl"))
