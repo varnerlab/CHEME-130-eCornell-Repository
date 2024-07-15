ca29360f7f09f97ac5602a0e983281549a85ff25
Jeffrey Varner <jdv27@cornell.edu>
2024-07-15 13:12:48 -0400

    hmmm
    hmmm

--- courses/CHEME-136/module-2/Include.jl
+++ courses/CHEME-136/module-2/Include.jl
@@ -3,7 +3,13 @@ const _ROOT = pwd();
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
 
+# check: do we have the required packahes loaded??
+using Pkg
+Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
+Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+
 # load external packages -
+using VLQuantitativeFinancePackage;
 using Distributions
 using DataFrames
 using CSV
@@ -13,7 +19,7 @@ using StatsBase
 using StatsPlots
 
 # load my codes -
-include(joinpath(_PATH_TO_SRC, "Types.jl"));
-include(joinpath(_PATH_TO_SRC, "Factory.jl"));
-include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+# include(joinpath(_PATH_TO_SRC, "Types.jl"));
+# include(joinpath(_PATH_TO_SRC, "Factory.jl"));
+# include(joinpath(_PATH_TO_SRC, "Compute.jl"));
 include(joinpath(_PATH_TO_SRC, "Files.jl"));
