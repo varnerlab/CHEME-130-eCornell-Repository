969a36a02f3e4a254c4472f9ae2d3a47af598bdb
Jeffrey Varner <jdv27@cornell.edu>
2024-07-18 12:21:17 -0400

    wow! super cool
    super cool

--- courses/CHEME-136/module-2/Include.jl
+++ courses/CHEME-136/module-2/Include.jl
@@ -21,7 +21,4 @@ using Colors
 using PrettyTables
 
 # load my codes -
-# include(joinpath(_PATH_TO_SRC, "Types.jl"));
-# include(joinpath(_PATH_TO_SRC, "Factory.jl"));
-# include(joinpath(_PATH_TO_SRC, "Compute.jl"));
 include(joinpath(_PATH_TO_SRC, "Files.jl"));
